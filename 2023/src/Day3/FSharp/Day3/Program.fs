c6d83a5c0d5c492394e099c25d1c2570a04f955f
moerwald <andreas.mewald@gmail.com>
2023-12-10 21:34:54 +0100

    2023 Day 3, Part 1 reformated, no functional changes.

--- 2023/src/Day3/FSharp/Day3/Program.fs
+++ 2023/src/Day3/FSharp/Day3/Program.fs
@@ -1,5 +1,4 @@
-﻿
-module Day3.Program
+﻿module Day3.Program
 
 type Coordinates = int * int
 
@@ -9,92 +8,100 @@ type Character =
     | Symbol of char
 
 type Bundle =
-    | Empty 
+    | Empty
     | Number of int
     | Symbol of string
 
-let addCoordinatesToCharacters (lines : string list) =
-    [for (y, line) in lines |> Seq.indexed do 
-        [for (x, c) in line |> Seq.indexed ->  
-            match c with
-            | c when System.Char.IsDigit(c) -> Coordinates (x,y), c |> string |> Digit
-            | '.' -> Coordinates (x,y), Character.Empty 
-            | _ -> Coordinates (x,y),  Character.Symbol c
-        ]
-     ]
+let addCoordinatesToCharacters (lines: string list) =
+    [ for (y, line) in lines |> Seq.indexed ->
+          [ for (x, c) in line |> Seq.indexed ->
+                match c with
+                | c when System.Char.IsDigit(c) -> Coordinates(x, y), c |> string |> Digit
+                | '.' -> Coordinates(x, y), Character.Empty
+                | _ -> Coordinates(x, y), Character.Symbol c ] ]
 
-let rec bundle (line: (Coordinates * Character ) list) =
+let rec bundle (line: (Coordinates * Character) list) =
     match line with
     | [] -> []
-    | (_ , Digit _ )::t ->
-        let adjacentDigits = line |> List.takeWhile (fun (c, e) -> 
-            match e with
-            | Digit _ -> true
-            | _ -> false)
+    | (_, Digit _) :: t ->
+        let adjacentDigits =
+            line
+            |> List.takeWhile (fun (c, e) ->
+                match e with
+                | Digit _ -> true
+                | _ -> false)
 
-        let nr = 
+        let nr =
             adjacentDigits
-            |> List.map ( fun (_, ch) -> match ch with | Digit d -> d | _ -> "")  
+            |> List.map (fun (_, ch) ->
+                match ch with
+                | Digit d -> d
+                | _ -> "")
             |> String.concat ""
             |> int
             |> Number
 
         let coordinates = adjacentDigits |> List.map fst
 
-        (coordinates, nr):: bundle(line |> List.skip (adjacentDigits.Length))
-    | (co, Character.Empty)::t -> ([co], Bundle.Empty)::bundle(line |> List.skip 1)
-    | (co, Character.Symbol s)::t -> ([co], Bundle.Symbol (s|> string))::bundle(line |> List.skip 1)
+        (coordinates, nr) :: bundle (line |> List.skip (adjacentDigits.Length))
+    | (co, Character.Empty) :: t -> ([ co ], Bundle.Empty) :: bundle (line |> List.skip 1)
+    | (co, Character.Symbol s) :: t -> ([ co ], Bundle.Symbol(s |> string)) :: bundle (line |> List.skip 1)
 
-let parseInput () = 
+let parseInput () =
     System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\\input_part1.txt"
     |> List.ofArray
-    |> addCoordinatesToCharacters 
+    |> addCoordinatesToCharacters
     |> List.map bundle
 
-let getSymbolCoordinates bundle = 
+let getSymbolCoordinates bundle =
     bundle
     |> List.collect id
-    |> List.filter(fun (_ , b) -> match b with | Bundle.Symbol _ -> true | _ -> false)
+    |> List.filter (fun (_, b) ->
+        match b with
+        | Bundle.Symbol _ -> true
+        | _ -> false)
     |> List.map fst
     |> List.collect id
 
-let getNumbers bundle = 
+let getNumbers bundle =
     bundle
     |> List.collect id
-    |> List.filter (fun (_, b) -> match b with | Bundle.Number _ -> true | _ -> false)
+    |> List.filter (fun (_, b) ->
+        match b with
+        | Bundle.Number _ -> true
+        | _ -> false)
 
-let isNumberAdjacentToSymbol symbols (x,y) =
-    let neighbors = [
-        (x-1, y-1); (x, y-1); (x+1, y-1);
-        (x-1, y);             (x+1, y);
-        (x-1, y+1); (x, y+1); (x+1, y+1);
-    ]
+let isNumberAdjacentToSymbol symbols (x, y) =
+    let neighbors =
+        [ (x - 1, y - 1)
+          (x, y - 1)
+          (x + 1, y - 1)
+          (x - 1, y)
+          (x + 1, y)
+          (x - 1, y + 1)
+          (x, y + 1)
+          (x + 1, y + 1) ]
 
     let symbolSet = Set.ofList symbols
     let neighborSet = Set.ofList neighbors
 
-    (Set.intersect symbolSet neighborSet)
-    |> Set.isEmpty
-    |> not
+    (Set.intersect symbolSet neighborSet) |> Set.isEmpty |> not
 
-let getValidNumbers bundle = 
+let getValidNumbers bundle =
     getNumbers bundle
-    |> List.filter 
-        (fun (col, nr) -> 
-                      col
-                      |> List.filter 
-                        (fun (x,y) -> 
-                            isNumberAdjacentToSymbol (getSymbolCoordinates bundle) (x,y))
-                      |> List.isEmpty
-                      |> not
-                 )
+    |> List.filter (fun (col, nr) ->
+        col
+        |> List.filter (fun (x, y) -> isNumberAdjacentToSymbol (getSymbolCoordinates bundle) (x, y))
+        |> List.isEmpty
+        |> not)
 
-let solvePart1 getInput = 
+let solvePart1 getInput =
     getInput ()
-    |> getValidNumbers 
-    |> List.map snd 
-    |> List.sumBy (fun n -> match n with | Bundle.Number n -> n | _ -> 0 )
+    |> getValidNumbers
+    |> List.map snd
+    |> List.sumBy (fun n ->
+        match n with
+        | Bundle.Number n -> n
+        | _ -> 0)
 
 solvePart1 parseInput |> printfn "Result %i"
-
-
--- 2023/src/Day3/FSharp/Day3Tests/Tests.fs
+++ 2023/src/Day3/FSharp/Day3Tests/Tests.fs
@@ -6,57 +6,54 @@ open FsUnit
 open Day3.Program
 
 
-let example = [
-    "467..114.."
-    "...*......"
-    "..35..633."
-    "......#..."
-    "617*......"
-    ".....+.58."
-    "..592....."
-    "......755."
-    "...$.*...."
-    ".664.598.."
-]
+let example =
+    [ "467..114.."
+      "...*......"
+      "..35..633."
+      "......#..."
+      "617*......"
+      ".....+.58."
+      "..592....."
+      "......755."
+      "...$.*...."
+      ".664.598.." ]
 
 let getParsed () =
-    example
-    |> addCoordinatesToCharacters
-    |> List.map bundle
+    example |> addCoordinatesToCharacters |> List.map bundle
 
 [<Fact>]
 let ``Parse numbers in line`` () =
-    let result = ["467..114.."] |> addCoordinatesToCharacters
-    result[0] |> should equal [
-       ((0,0), Digit "4")
-       ((1,0), Digit "6")
-       ((2,0), Digit "7")
-       ((3,0), Character.Empty)
-       ((4,0), Character.Empty)
-       ((5,0), Digit "1")
-       ((6,0), Digit "1")
-       ((7,0), Digit "4")
-       ((8,0), Character.Empty)
-       ((9,0), Character.Empty)
-       ]
+    let result = [ "467..114.." ] |> addCoordinatesToCharacters
+
+    result[0]
+    |> should
+        equal
+        [ ((0, 0), Digit "4")
+          ((1, 0), Digit "6")
+          ((2, 0), Digit "7")
+          ((3, 0), Character.Empty)
+          ((4, 0), Character.Empty)
+          ((5, 0), Digit "1")
+          ((6, 0), Digit "1")
+          ((7, 0), Digit "4")
+          ((8, 0), Character.Empty)
+          ((9, 0), Character.Empty) ]
 
 
 [<Fact>]
 let ``bundle line`` () =
 
-    let result = 
-        ["467..114.."]
-        |> addCoordinatesToCharacters
-        |> List.map bundle
+    let result = [ "467..114.." ] |> addCoordinatesToCharacters |> List.map bundle
 
     result[0]
-    |> should equal  [
-        ([(0, 0); (1, 0); (2, 0)], Number 467);
-        ([(3, 0)], Empty);
-        ([(4, 0)], Empty);
-        ([(5, 0); (6, 0); (7, 0)], Number 114);
-        ([(8, 0)], Empty);
-        ([(9, 0)], Empty)]
+    |> should
+        equal
+        [ ([ (0, 0); (1, 0); (2, 0) ], Number 467)
+          ([ (3, 0) ], Empty)
+          ([ (4, 0) ], Empty)
+          ([ (5, 0); (6, 0); (7, 0) ], Number 114)
+          ([ (8, 0) ], Empty)
+          ([ (9, 0) ], Empty) ]
 
 [<Fact>]
 let ``solve example`` () =
